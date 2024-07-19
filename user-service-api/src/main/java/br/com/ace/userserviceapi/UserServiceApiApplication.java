536418a7dccc301f5bedc65f854ccdd6a03a372f
ace0777 <luishhasantos@gmail.com>
2024-07-19 19:22:04 -0300

    feat: add user repository

--- user-service-api/src/main/java/br/com/ace/userserviceapi/UserServiceApiApplication.java
+++ user-service-api/src/main/java/br/com/ace/userserviceapi/UserServiceApiApplication.java
@@ -1,10 +1,18 @@
 package br.com.ace.userserviceapi;
 
+import br.com.ace.userserviceapi.entity.User;
+import br.com.ace.userserviceapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import models.enums.ProfileEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Set;
 
 @SpringBootApplication
-public class UserServiceApiApplication {
+@RequiredArgsConstructor
+public class UserServiceApiApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApiApplication.class, args);
