2cbaf51a20af850498f02209b0433565afea711c
ace0777 <luishhasantos@gmail.com>
2024-07-20 13:20:23 -0300

    feat: add endpoint find by id

---
+++ user-service-api/src/main/java/br/com/ace/userserviceapi/service/UserService.java
@@ -0,0 +1,18 @@
+package br.com.ace.userserviceapi.service;
+
+import br.com.ace.userserviceapi.entity.User;
+import br.com.ace.userserviceapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findById(final String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
+
